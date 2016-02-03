3233e73f03fe0ab12bd097b04c52696ae8412694
Blair Aitcheson <baitch1996@gmail.com>
2016-02-03 15:51:38 +0000

    Finished chapter 6, changed admin.py to make Pages inline on the Category model

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+#class PageAdmin(admin.ModelAdmin):
+#    fieldsets = [
+#        (None, {'fields': ['category']}),
+#        ('Page info', {'fields': ['title', 'url']}),
+#    ]
+
+class PageInLine(admin.TabularInline):
+    model = Page
+    extra = 0
+
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,        {'fields': ['name']}),
+        ('Popularity info', {'fields': ['views', 'likes'],
+                             'classes': ['collapse']}),
+    ]
+    inlines = [PageInLine]
+
+admin.site.register(Category, CategoryAdmin)
+#admin.site.register(Page, PageAdmin)
 
 # Register your models here.
