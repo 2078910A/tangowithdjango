4534a8b62fd4d41b25da37d320e152a8357b6f57
Blair Aitcheson <baitch1996@gmail.com>
2016-02-10 12:50:05 +0000

    Changed hardcoded links in templates to use Django's {% url %} template tag

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -3,8 +3,11 @@ from rango import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       #ex: /rango/
                        url(r'^about/', views.about, name='about'),
+                       #ex: /rango/about/
                        url(r'^category/(?P<category_name_url>[\w\-]+)/$',
                            views.category, name='category'),
+                       #ex: /rango/category/other-frameworks/
                        )
 
